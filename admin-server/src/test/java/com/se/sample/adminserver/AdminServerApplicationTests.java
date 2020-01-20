2b5adb28252eca35303dc371cef5f298697d8a37
Evgeniy Skiba <eskiba@s-pro.io>
2020-01-20 18:04:27 +0200

    New. added clean admin server

---
+++ admin-server/src/test/java/com/se/sample/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se.sample.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
